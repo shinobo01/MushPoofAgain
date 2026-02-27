c5f641f3cec09d8e48ac08a6028ab39b525b7d13
shinobo01 <162945322+shinobo01@users.noreply.github.com>
2026-02-26 23:26:25 -0800

    Update build.gradle.kts to 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,19 @@ version = "4.1.6"
 
 repositories {
     mavenLocal()
-    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven(url = "https://repo.papermc.io/repository/maven-public/")
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks.withType<Test> {
